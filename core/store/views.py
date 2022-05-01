a6576f8f3fdb84255ecd66c5dbe533598d8d4b5e
HP <lagner@gmail.com>
2022-05-01 08:34:55 +0300

    Update views.py in store app
    add view function - index()

--- core/store/views.py
+++ core/store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import request, HttpResponse
+
+
+def index(request):
+    return HttpResponse('Здесь будет магазин')
 
 # Create your views here.
